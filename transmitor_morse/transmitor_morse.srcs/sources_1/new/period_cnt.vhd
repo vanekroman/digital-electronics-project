43638f15dd861e5f9aada9763b5efc68b8c17f5f
Vaněk Roman (240707) <240707@vutbr.cz>
2023-04-05 12:13:30 +0200

    created vivado project

---
+++ transmitor_morse/transmitor_morse.srcs/sources_1/new/period_cnt.vhd
@@ -0,0 +1,72 @@
+----------------------------------------------------------
+--
+--! @title Clock enable
+--! @author Roman Vanek
+--!
+--! @copyright (c) 2023 Roman Vanek
+--! This work is licensed under the terms of the MIT license
+--!
+--! Reads period of input pulse and output boolean value, if the period
+--! is within defined value.
+--
+-- Hardware: Nexys A7-50T, xc7a50ticsg324-1L
+-- Software: TerosHDL, Vivado 2020.2, EDA Playground
+--
+----------------------------------------------------------
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all; -- Package for arithmetic operations
+
+----------------------------------------------------------
+-- Entity declaration for clock enable
+----------------------------------------------------------
+
+entity period_cnt is
+  generic (
+    g_MAX : natural := 5 --! Number of clk pulses to generate one enable signal period
+  );                       -- Note that there IS a semicolon between generic and port sections
+  port (
+    clk : in    std_logic; --! Main clock
+    rst : in    std_logic; --! High-active synchronous reset
+    ce  : out   std_logic  --! Clock enable pulse signal
+  );
+end entity period_cnt;
+
+------------------------------------------------------------
+-- Architecture body for clock enable
+------------------------------------------------------------
+
+architecture behavioral of period_cnt is
+
+  -- Local counter
+  signal sig_cnt : natural;
+
+begin
+
+  --------------------------------------------------------
+  -- p_clk_enable:
+  -- Generate clock enable signal. By default, enable signal
+  -- is low and generated pulse is always one clock long.
+  --------------------------------------------------------
+  p_clk_enable : process (clk) is
+  begin
+
+    if rising_edge(clk) then              -- Synchronous process
+      if (rst = '1') then                 -- High-active reset
+        sig_cnt <= 0;                     -- Clear local counter
+        ce      <= '0';                   -- Set output to low
+
+      -- Test number of clock periods
+      elsif (sig_cnt >= (g_MAX - 1)) then
+        sig_cnt <= 0;                     -- Clear local counter
+        ce      <= '1';                   -- Generate clock enable pulse
+      else
+        sig_cnt <= sig_cnt + 1;
+        ce      <= '0';
+      end if;
+    end if;
+
+  end process p_clk_enable;
+
+end architecture behavioral;
