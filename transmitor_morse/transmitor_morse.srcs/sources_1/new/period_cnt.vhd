c0f031b0f62ff068024050629d7039b66618c830
Vaněk Roman (240707) <240707@vutbr.cz>
2023-04-12 12:52:04 +0200

    period_cnt changed romis

--- transmitor_morse/transmitor_morse.srcs/sources_1/new/period_cnt.vhd
+++ transmitor_morse/transmitor_morse.srcs/sources_1/new/period_cnt.vhd
@@ -24,14 +24,15 @@ library ieee;
 
 entity period_cnt is
   generic (
-    g_MAX : natural := 5 --! Number of clk pulses to generate one enable signal period
+    g_dot_length  : natural := 5 --! Number of clk pulses to generate one enable signal period
   );
   port (
-    clk    : in  std_logic; --! Main clock
-    rst    : in  std_logic; -- Button
-    input  : in  std_logic; -- Button
-    output : out std_logic;  -- Button
-    cnt    : out std_logic_vector(31 downto 0)
+    clk      : in  std_logic; --! Main clock
+    i_space  : in  std_logic; -- Button
+    i_logic  : in  std_logic; -- Button
+    o_morse  : out std_logic_vector(3 downto 0); -- Full morse code according to o_cnt_n
+    o_cnt    : out std_logic_vector(2 downto 0); -- Number of input signals 0 - 4
+    o_read   : out std_logic
   );
 end entity period_cnt;
 
@@ -42,7 +43,9 @@ end entity period_cnt;
 architecture behavioral of period_cnt is
 
   -- Local counter
-  signal sig_cnt : unsigned(31 downto 0) := (others => '0'); --! Local counter
+  signal sig_counter : unsigned(31 downto 0) := (others => '0'); --! Local counter
+  signal sig_morse   : unsigned(3 downto 0)  := (others => '0'); --! Local counter
+  signal sig_cnt     : natural := 0; --! Local counter
 
 begin
 
@@ -54,19 +57,36 @@ begin
   p_clk_enable : process (clk) is
   begin
 
-    if rising_edge(clk) then              -- Synchronous process
+    if rising_edge(clk) then -- Synchronous process
       
-      if (input = '1') then
-        sig_cnt <= sig_cnt + 1;
-     
-      elsif (rst = '1') then
-        sig_cnt <= (others => '0');
+      if (i_logic = '1') then
+        o_read <= '0';                      -- Dont read yet
+        sig_counter <= sig_counter + 1;
+      elsif (sig_counter > 0) then -- end of logic signal
+        -- asigh DASH or DOT to correct position
+        if(sig_counter > g_dot_length) then -- DASH
+            sig_morse(sig_cnt) <= '1';
+        else                                -- DOT
+            sig_morse(sig_cnt) <= '0';
+        end if;
+        -- include new bit definition (Dot / Dash) for whole letter
+        sig_cnt <= sig_cnt + 1;    
+        o_cnt <= std_logic_vector(to_unsigned(sig_cnt, 3)); 
+                
+      end if;
+      
+      if (i_space = '1') then
+        o_morse <= std_logic_vector(sig_morse);
+        o_read <= '1';                      -- LETTER is completed (init read stage)
         
+        sig_counter <= (others => '0');     -- Reset all values
+        sig_morse <= (others => '0');
+        sig_cnt <= 0;
+        -- TODO: use only o_cnt
+        o_cnt <= std_logic_vector(to_unsigned(sig_cnt, 3)); 
       end if;
       
     end if;
 
   end process p_clk_enable;
-  
-  cnt <= std_logic_vector(sig_cnt);
 end architecture behavioral;
